ed6f06aaf8621fd72782834e98e74af583a2946f
kemalgunduz79 <115525068+kemalgunduz79@users.noreply.github.com>
2022-12-08 09:31:07 +0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,44 @@
+<script>
+        document.getElementById("yellow").onclick = function () {
+          document.getElementById("all").style.backgroundColor = "white";
+          document.getElementById("zemin").style.backgroundColor = "yellow";
+          document.getElementById("ucgen").style.borderRightColor = "yellow";
+          document.getElementById("fener").style.display = "block";
+        };
+        document.getElementById("red").onclick = function () {
+          document.getElementById("all").style.backgroundColor = "white";
+          document.getElementById("zemin").style.backgroundColor = "red";
+          document.getElementById("ucgen").style.borderRightColor = "red";
+          document.getElementById("fener").style.display = "block";
+        };
+        document.getElementById("green").onclick = function () {
+          document.getElementById("all").style.backgroundColor = "white";
+          document.getElementById("zemin").style.backgroundColor = "green";
+          document.getElementById("ucgen").style.borderRightColor = "green";
+          document.getElementById("fener").style.display = "block";
+        };
+        document.getElementById("blue").onclick = function () {
+          document.getElementById("all").style.backgroundColor = "white";
+          document.getElementById("zemin").style.backgroundColor = "blue";
+          document.getElementById("ucgen").style.borderRightColor = "blue";
+          document.getElementById("fener").style.display = "block";
+        };
+        document.getElementById("orange").onclick = function () {
+          document.getElementById("all").style.backgroundColor = "white";
+          document.getElementById("zemin").style.backgroundColor = "orange";
+          document.getElementById("ucgen").style.borderRightColor = "orange";
+          document.getElementById("fener").style.display = "block";
+        };
+        document.getElementById("brown").onclick = function () {
+          document.getElementById("all").style.backgroundColor = "white";
+          document.getElementById("zemin").style.backgroundColor = "brown";
+          document.getElementById("ucgen").style.borderRightColor = "brown";
+          document.getElementById("fener").style.display = "block";
+        };
+        document.getElementById("kapat").onclick = function () {
+          document.getElementById("all").style.backgroundColor = "black";
+          document.getElementById("zemin").style.backgroundColor = "black";
+          document.getElementById("ucgen").style.borderRightColor = "black";
+          document.getElementById("fener").style.display = "none";
+        };
+      </script>
